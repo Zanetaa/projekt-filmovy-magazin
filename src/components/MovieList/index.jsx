46aea4a37a2c7a33b6924ebe2bb27a8b63573952
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-04-10 10:16:18 +0200

    dílčí úkoly č.11 a 12

--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './style.css';
 import movies from '../../movie-database.js';
+import { NavLink } from 'react-router-dom';
 
 const MovieList = () => {
   return (
     <div className="container">
-      <div className="movie"></div>
+      <div className="movie-list">
+        {movies.map((movie) => {
+          return (
+            <NavLink
+              to={`/movies/${movie.id}`}
+              className={({ isActive, isPending }) =>
+                isPending ? 'pending' : isActive ? 'active' : ''
+              }
+              key={movie.id}
+            >
+              {movie.title}
+            </NavLink>
+          );
+        })}
+      </div>
     </div>
   );
 };
