3a71b38f79bdc00ea2a26ceaba0f05fe0f944583
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-04-11 19:42:06 +0200

    oprava chyb v komponentě movie

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -5,31 +5,21 @@ import { useParams } from 'react-router-dom';
 import { movies } from '../../movie-database.js';
 
 const Movie = () => {
-  const { movieId } = useParams();
-  const chosenMovie = movies.find((movie) => movie.id === movieId);
+  const { id } = useParams();
+  const chosenMovie = movies.find((movie) => movie.id === Number(id));
 
   return (
     <div className="container">
       <div className="movies-container">
-        <h1>
-          {chosenMovie.title} ({chosenMovie.movieId})
-        </h1>
+        <h1>{chosenMovie.title}</h1>
         <br />
         <img src={chosenMovie.poster} />
       </div>
       <div className="movie-list">
-        <p>
-          Year: {chosenMovie.year}({chosenMovie.chosenId})
-        </p>
-        <p>
-          Rating:{chosenMovie.rating}({chosenMovie.chosenId})
-        </p>
-        <p>
-          Director:{chosenMovie.director}({chosenMovie.chosenId})
-        </p>
-        <p>
-          Story line:{chosenMovie.storyline}({chosenMovie.chosenId})
-        </p>
+        <p>Year: {chosenMovie.year}</p>
+        <p>Rating: {chosenMovie.rating}</p>
+        <p>Director: {chosenMovie.director}</p>
+        <p>Story line: {chosenMovie.storyline}</p>
         <span className="container">
           <p>
             Cast:
